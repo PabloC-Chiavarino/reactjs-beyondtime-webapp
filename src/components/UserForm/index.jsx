2a88a761c820a01d26a92c8564a78b196ca64672
Pablo C. Chiavarino <pchiavarino89@gmail.com>
2025-02-18 11:52:25 -0300

    feat: add ring animation on FormData component

--- src/components/UserForm/index.jsx
+++ src/components/UserForm/index.jsx
@@ -1,27 +1,64 @@
 import { useState, useEffect } from 'react'
 import './styles.css'
 
-const UserForm = () => {
+const UserForm = ({ onComplete }) => {
+  const [ringAnimation, setRingAnimation] = useState(false)
   const [showForm, setShowForm] = useState(false)
 
   useEffect(() => {
-    setTimeout(() => {
+    const timeout = setTimeout(() => {
       setShowForm(true)
     }, 850)
+
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [])
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setRingAnimation(true)
+    }, 2500)
+
+    const interval = setInterval(() => {
+      setRingAnimation(prevState => !prevState)
+    }, 2500)
+
+    return () => {
+      clearInterval(interval)
+      clearTimeout(timeout)
+    }
   }, [])
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log('Datos enviados') // debug
+    const [name, birthdate] = e.target
+    console.log(name.value, birthdate.value) // debug
+    onComplete()
   }
 
   return (
     <div className='userData__container fadeIn'>
+      <div className='fadeIn userData__container--ring' />
+      <div className={`userData__container--secondRing ${ringAnimation ? 'ringAnimation' : ''}`} />
+      <div className={`userData__container--thirdRing ${ringAnimation ? 'ringAnimation--reverse' : ''}`} />
       <h1 className='userData__title'>Ingresa tus datos</h1>
       {showForm && (
         <form className='userData__form' onSubmit={handleSubmit}>
-          <input className='fadeIn' type='text' placeholder='Nombre' required />
-          <input className='fadeIn' type='date' placeholder='Fecha de nacimiento' required />
+          <input
+            className='fadeIn'
+            type='text'
+            placeholder='Nombre'
+            required
+          />
+          <input
+            className='fadeIn'
+            type='date'
+            min='1900-01-01'
+            max={new Date().toISOString().split('T')[0]}
+            placeholder='Fecha de nacimiento'
+            required
+          />
           <button className='fadeIn'>Enviar</button>
         </form>
       )}
