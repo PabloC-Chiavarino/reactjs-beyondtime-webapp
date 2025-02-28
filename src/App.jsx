9d0218fcab89b0ee1f6b436ed85e92942a483a94
Pablo C. Chiavarino <pchiavarino89@gmail.com>
2025-02-28 12:42:36 -0300

    feat: add on click callback to Logo component to start again

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ function App () {
 
   return (
     <div className='App'>
-      <Logo />
+      <Logo onClick={() => setStep('welcome')} />
       {step === 'welcome' && <FlowScreen type={step} onFinish={() => handleStep()} />}
       {userControl.current === true && <ForwardBtn handleClick={handleStep} />}
 
--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,9 +1,9 @@
 import { timeImg } from '../../assets/img'
 import './styles.css'
 
-const Logo = () => {
+const Logo = ({ onClick }) => {
   return (
-    <div className='logo__container fadeIn'>
+    <div className='logo__container fadeIn' onClick={onClick}>
       <h1>Beyond<span>Time</span></h1>
       <img src={timeImg} alt='logo' />
     </div>
