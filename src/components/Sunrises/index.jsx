c10a5e0fb8b35fa154be65f8e6770600802c1a0b
Pablo C. Chiavarino <pchiavarino89@gmail.com>
2025-03-01 18:45:22 -0300

    feat: add animation to Sunrises component

--- src/components/Sunrises/index.jsx
+++ src/components/Sunrises/index.jsx
@@ -6,6 +6,7 @@ const Sunrises = () => {
       <h1 className='sunrises__title'>Amaneceres</h1>
       <p className='sunrises__textA'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur repudiandae labore temporibus explicabo molestiae numquam, modi eum accusantium non quod porro nemo! Adipisci, eveniet repudiandae? Assumenda, iure. Vero, alias fuga!</p>
       <div className='sunrises__subcontainer'>
+        <div className='sunrises__sky' />
         <div className='sunrises__sun' />
       </div>
       <p className='sunrises__textB'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, recusandae ex rem quae fugit iste maiores laudantium vitae, ipsa, sit reprehenderit beatae pariatur aperiam dignissimos rerum ut hic nihil laboriosam?</p>
