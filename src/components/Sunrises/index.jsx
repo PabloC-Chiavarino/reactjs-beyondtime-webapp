40f9a71171f00edfd9541d0700e2241c764ba3b7
Pablo C. Chiavarino <pchiavarino89@gmail.com>
2025-03-01 19:22:22 -0300

    feat: add heat svg animation to Sunrises component

--- src/components/Sunrises/index.jsx
+++ src/components/Sunrises/index.jsx
@@ -8,6 +8,14 @@ const Sunrises = () => {
       <div className='sunrises__subcontainer'>
         <div className='sunrises__sky' />
         <div className='sunrises__sun' />
+        <svg width='0' height='0'>
+          <filter id='heatDistortion'>
+            <feTurbulence type='turbulence' baseFrequency='0.01 0.08' numOctaves='2' result='turbulence'>
+              <animate attributeName='baseFrequency' dur='0.2s' values='0.01 0.08; 0.02 0.1' repeatCount='indefinite' />
+            </feTurbulence>
+            <feDisplacementMap in='SourceGraphic' in2='turbulence' scale='15' />
+          </filter>
+        </svg>
       </div>
       <p className='sunrises__textB'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, recusandae ex rem quae fugit iste maiores laudantium vitae, ipsa, sit reprehenderit beatae pariatur aperiam dignissimos rerum ut hic nihil laboriosam?</p>
     </div>
